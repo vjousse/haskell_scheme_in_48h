4d0693ae0a69afae3099e72d2af362ed68b2390a
Vincent Jousse <vincent@jousse.org>
2012-03-17 15:19:58 +0100

    Parse simple lisp symbols

--- main.hs
+++ main.hs
@@ -1,9 +1,18 @@
 module Main where
 import System.Environment
+import Text.ParserCombinators.Parsec hiding (spaces)
 
-main::IO()
-main = do
-    args <- getArgs
-    putStrLn("Enter your name please")
-    line <- getLine
-    putStrLn(line)
+main :: IO()
+main = do args <- getArgs
+          putStrLn (readExpr (args !! 0))
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+readExpr :: String -> String
+readExpr input = case parse (spaces >> symbol) "lisp" input of
+    Left err -> "No match: " ++ show err
+    Right val -> "Found value"
+
+spaces :: Parser ()
+spaces = skipMany1 space
